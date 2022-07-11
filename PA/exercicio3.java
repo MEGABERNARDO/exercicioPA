c1fc02c2ce088f963b1e8256ad2e74a0a56c07bd
MEGABERNARDO <107855519+MEGABERNARDO@users.noreply.github.com>
2022-07-11 11:51:17 -0300

    Add files via upload

---
+++ PA/exercicio3.java
@@ -0,0 +1,15 @@
+public class exercicio3 {
+    public  static  void  main ( String [] args ) {
+     int  a , b ;
+     flutuar  c , d ;
+
+      a = ( 3 - 2 - 1 + 2 + 1 + 3 );
+      b = ( 2 * 3 - 4 * 5 );
+      c = ( 2 + 6 - 3/7 * 9 ) ;
+      d = ( 3 % 4-8 ) ;
+      Sistema . fora . println ( "Resultado A: " + a );
+      Sistema . fora . println ( "Resultado B: " + b );
+      Sistema . fora . println ( "Resultado C: " + c );
+      Sistema . fora . println ( "Resultado D:" + d );
+    }
+}    
